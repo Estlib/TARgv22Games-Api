665d1cb1390c14c288ee932183a92a240c9220c1
Estlib <75094683+Estlib@users.noreply.github.com>
2023-12-05 16:43:21 +0200

    As an app developer I want to get full details of any given game to show up in my app. [Delivers #186567093]

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const app = require('express')();
 const port = 8080
 const swaggerUI = require('swagger-ui-express');
-const swaggerDocument = require('./docs/swagger.json');
+const yamljs = require('yamljs');
+//const swaggerDocument = require('./docs/swagger.json');
+const swaggerDocument = yamljs.load('./docs/swagger.yaml');
+
+const games = [
+    {id: 1, name: "Team Fortress 2", price: "free", rating: 5},
+    {id: 2, name: "Cyberpunk 2077", price: 69.99, rating: 3},
+    {id: 3, name: "Paladins: Champions of the Realm", price: "free", rating: 4},
+    {id: 4, name: "Super Mario Bros. Wonder", price: 69.99, rating: 5},
+    {id: 5, name: "Sonic the Hedgehog", price: 1.99, rating: 4},
+    {id: 6, name: "God of War 3", price: 29.99, rating: 4},
+    {id: 7, name: "Half:Life", price: "free", rating: 5},
+    {id: 8, name: "Portal", price: 2.99, rating: 5},
+    {id: 9, name: "Goat Simulator", price: 12.99, rating: 3}    
+]
 
 app.get('/games', (req, res) =>{
-    res.send(["Team Fortress 2", "Cyberpunk 2077", "Paladins: Champions of the Realm", "Super Mario Bros. Wonder"])
+    res.send(games)
+})
+
+app.get('/games/:id', (req, res) =>{
+    if(typeof games[req.params.id - 1] === 'undefined')
+    {
+        return res.status(404).send({error: "Game not found"})
+    }
+    res.send(games[req.params.id - 1])
 })
 
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
