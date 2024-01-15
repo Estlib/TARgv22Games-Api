01bbb78ff95b7cb7cc76f2913804bc609181e314
Estlib <75094683+Estlib@users.noreply.github.com>
2024-01-15 12:43:22 +0200

    tunnicommit

--- routes/app_routes.js
+++ routes/app_routes.js
@@ -6,4 +6,6 @@ module.exports = (app) => {
         .post(gamesController.createNew) // create a new game
     app.route("/games/:id")     
         .get(gamesController.getById)    // get a game by id
+        .put(gamesController.updateById) // change info of a game by id
+        .delete(gamesController.deleteById) // delete a game by id
 }
